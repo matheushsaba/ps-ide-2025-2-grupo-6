6abd5c92104249b0ddc20666aa554ada579603dd
Pedro Muller Legnaghi <157427799+PedroMLegnaghi@users.noreply.github.com>
2025-09-28 19:43:58 -0300

    Adicionei funcionalidade ao formulário de 'contate-nos', atualmente o formulário somente pega os dados colocados no input, não os armazena. Adicionei um validador de email básico por meio de regex, que não permite o usuário enviar um endereço de email sintaticamente inválido. Para melhor modularidade, optei por separar a função do evento de envio do formulário da atribuição direta do evento ao formulário, ou seja, a função de envio está em 'form.js' e a atribuição desse evento ao formulário está no script principal 'script.js'. No arquivo 'index.html', somente alterei uma propriedade na importação do script, para que possa ser possível trabalhar com módulos (usando ES6 modules). Formulario nao reseta a página após envio, somente limpa os inputs utilizados

---
+++ script.js
@@ -0,0 +1,9 @@
+import { eventoFormulario } from "./form.js";
+
+//FORMULÁRIO ------------------------------------------------------------------------------------------------------------
+
+const formulario = document.querySelector("#formulario_contato");
+//evento de enviar o formulário
+formulario.addEventListener("submit", eventoFormulario);
+
+//------------------------------------------------------------------------------------------------------------------------
